17145d1333f93a5d2bf3f6d60fe0280938c1b0e2
otov <voicehovicso@gmail.com>
2024-11-28 13:27:18 +0200

    bb

--- jsonnn.py
+++ jsonnn.py
@@ -1,6 +1,6 @@
 import json
 
-with open ("jsonn.json", "r",encoding="utf-8") as file:
+with open ("Jsonn.json", "r",encoding="utf-8-sig") as file:
     data=json.load(file)
     print(data)
 print("datnes nosaukums ir: ",file.name)
