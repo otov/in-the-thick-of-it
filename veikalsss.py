cf20ccf67a3e6c9aa13d3d843471d191a9cbbf64
otov <voicehovicso@gmail.com>
2024-12-05 13:40:14 +0200

    786786578

--- veikalsss.py
+++ veikalsss.py
@@ -1,1 +1,13 @@
 import requests
+
+#url = "https://fakestoreapi.com/products?limit=5"
+url = "https://fakestoreapi.com/products/category/jewelery"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    print(data)
+except requests.exeptions.RequestExeption as e:
+    print(f"Error fetching data: {e}")
+
